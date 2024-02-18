12ab9a452de604c6d3a87552614cf40dc3f9a8d7
Ahammed Shaheer <shaheerbinmuhammed6@gmail.com>
2024-02-18 13:41:11 +0530

    changed folder structure of admin and user

--- src/app/modules/artist/components/artist-song-list/artist-song-list.component.ts
+++ src/app/modules/artist/components/artist-song-list/artist-song-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ArtistService } from '../../services/artist.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-artist-song-list',
@@ -7,18 +8,34 @@ import { ArtistService } from '../../services/artist.service';
   styleUrls: ['./artist-song-list.component.css'],
 })
 export class ArtistSongListComponent implements OnInit {
-  songs:any;
-  constructor(private artistServ:ArtistService){}
+  songs: any;
+  idSong: any;
+  message: any;
+  success!: Boolean;
+  showDeleteDiv: Boolean = false;
+  constructor(private artistServ: ArtistService, private router: Router) {}
   ngOnInit(): void {
-    this.artistServ.artistGetSongs().subscribe((res)=>{
+    this.artistServ.artistGetSongs().subscribe((res) => {
       console.log(res);
-      this.songs=res;
-    })
+      this.songs = res.songs;
+      this.message = res.message;
+      this.success = res.success;
+    });
   }
-  deleteSong(songId:any){
-    console.log('delete btn clicked')
-    this.artistServ.artistDeleteSong(songId).subscribe((res)=>{
-      console.log(res)
-    })
+  showDeleteConfirmation(songId: any) {
+    this.showDeleteDiv = true;
+    console.log(songId);
+    this.idSong = songId;
+  }
+  deleteSong() {
+    try {
+      console.log('delete btn clicked');
+      this.artistServ.artistDeleteSong(this.idSong).subscribe((res) => {
+        console.log(res);
+      });
+      this.showDeleteDiv = false;
+    } catch (err) {
+      console.log(err);
+    }
   }
 }
